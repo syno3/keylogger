27fd37d43c399d430266c771654f64d681590637
syno3 <sinotechxvista@gmail.com>
2021-08-05 10:23:52 +0300

    bat update

--- script.bat
+++ script.bat
@@ -1,3 +1,3 @@
 @echo off
-start "c:\main.py"
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "c:\main.py"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
